ae493c7bae92df238add55c8dd656668f0560c9e
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2023-03-07 15:11:53 +0700

    Update bottom_nav_bar_widget.dart

--- lib/bottom_nav_bar/bottom_nav_bar_widget.dart
+++ lib/bottom_nav_bar/bottom_nav_bar_widget.dart
@@ -3,18 +3,10 @@ import 'package:let_tutor/flutter_flow/flutter_flow_theme.dart';
 
 class BottomNavBarWidget extends StatelessWidget {
   const BottomNavBarWidget({
-    // this.fabLocation = FloatingActionButtonLocation.endDocked,
     this.shape = const CircularNotchedRectangle(),
   });
 
-  // final FloatingActionButtonLocation fabLocation;
   final NotchedShape? shape;
-
-  // static final List<FloatingActionButtonLocation> centerLocations =
-  //     <FloatingActionButtonLocation>[
-  //   FloatingActionButtonLocation.centerDocked,
-  //   FloatingActionButtonLocation.centerFloat,
-  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +15,21 @@ class BottomNavBarWidget extends StatelessWidget {
       surfaceTintColor: FlutterFlowTheme.of(context).primaryColor,
       color: FlutterFlowTheme.of(context).primaryColor,
       child: IconTheme(
-        data: IconThemeData(
-            color: FlutterFlowTheme.of(context).primaryColor, size: 25),
+        data: IconThemeData(color: Colors.indigo),
         child: Row(
           children: <Widget>[
             IconButton(
               icon: const Icon(Icons.search),
               onPressed: () {},
             ),
-            // if (centerLocations.contains(fabLocation)) const Spacer(),
             IconButton(
               tooltip: 'Search',
               icon: const Icon(Icons.schedule),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: 'Favorite',
+              icon: const Icon(Icons.sticky_note_2_outlined),
               onPressed: () {},
             ),
             IconButton(
