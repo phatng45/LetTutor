90025e3c819dd45e374f9841880d5c9d169ba84d
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2023-03-11 18:10:26 +0700

    Update courses_page_widget.dart

--- lib/courses_page/courses_page_widget.dart
+++ lib/courses_page/courses_page_widget.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:let_tutor/components/best_divider_widget.dart';
 import 'package:let_tutor/flutter_flow/flutter_flow_theme.dart';
 import 'package:let_tutor/flutter_flow/flutter_flow_util.dart';
-import 'package:let_tutor/models/class_review_status_widget.dart';
-import 'package:let_tutor/models/class_schedule_status_widget.dart';
-import 'package:let_tutor/models/tutor_general_info_widget.dart';
 
 import 'courses_page_model.dart';
 
@@ -53,48 +49,81 @@ class _CoursesPageWidgetState extends State<CoursesPageWidget> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 1.0,
-                decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).secondaryBackground,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 4.0,
-                          color: Color(0x33000000),
-                          offset: Offset(0.0, 2.0),
-                          spreadRadius: 2.0,
-                        )
-                      ],
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          TutorGeneralInfoWidget(hasRating: false),
-                          BestDividerWidget(
-                            title: '',
-                          ),
-                          ClassScheduleStatus(),
-                          ClassScheduleStatus(),
-                          ClassScheduleStatus(),
-                        ],
-                      ),
-                    ),
-                  ),
+              CourseGeneralInfo(),
+              CourseGeneralInfo(),
+              CourseGeneralInfo(),
+              CourseGeneralInfo(),
+              CourseGeneralInfo(),
+            ],
+          ),
+        ));
+  }
+}
+
+class CourseGeneralInfo extends StatelessWidget {
+  const CourseGeneralInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 1.0,
+      decoration: BoxDecoration(
+        color: FlutterFlowTheme.of(context).secondaryBackground,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: FlutterFlowTheme.of(context).secondaryBackground,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 4.0,
+                color: Color(0x33000000),
+                offset: Offset(0.0, 2.0),
+                spreadRadius: 2.0,
+              )
+            ],
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.network(
+                    'https://picsum.photos/seed/437/600',
+                    height: 250,
+                    width: 1000,
+                    fit: BoxFit.cover),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Life in the Internet Age',
+                        style: FlutterFlowTheme.of(context).title1.override(
+                            fontFamily:
+                                FlutterFlowTheme.of(context).title1Family,
+                            // fontWeight: FontWeight.w500,
+                            color: Colors.indigo)),
+                    Text(
+                        'Let\'s discuss how technology is changing the way we live' + '\n',
+                    style: FlutterFlowTheme.of(context).bodyText1,
+                    maxLines: 2,),
+                    Text('Intermediate • 9 Lessons',
+                        style: FlutterFlowTheme.of(context).subtitle1),
+                  ],
                 ),
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
